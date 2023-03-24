4c94ee62c0d31da2ca3053a43875c83999df8232
Jardejane <106411484+Jardejane@users.noreply.github.com>
2023-03-24 20:32:39 -0300

    esqueci de configurar o nome api

--- src/database/api.js
+++ src/database/api.js
@@ -1,14 +1,16 @@
 import axios from "axios";
-axios.defaults.baseURL = "https://buddycare.onrender.com/";
-axios.defaults.headers.post["Content-Type"] = "application/json";
-axios.defaults.headers["Access-Control-Allow-Credentials"] = true;
-axios.defaults.headers["Access-Control-Allow-Origin"] =
+
+const api = axios;
+
+api.defaults.baseURL = "https://buddycare.onrender.com/";
+api.defaults.headers.post["Content-Type"] = "application/json";
+api.defaults.headers["Access-Control-Allow-Credentials"] = true;
+api.defaults.headers["Access-Control-Allow-Origin"] =
   "https://front-neon-ten.vercel.app";
-axios.defaults.headers["Access-Control-Allow-Methods"] =
+api.defaults.headers["Access-Control-Allow-Methods"] =
   "GET,PUT,POST,DELETE,PATCH,OPTIONS";
-axios.defaults.headers["Access-Control-Allow-Headers"] =
+api.defaults.headers["Access-Control-Allow-Headers"] =
   "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json";
-const api = axios.create({});
 
 api.interceptors.request.use(
   (config) => {
@@ -21,16 +23,15 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
-axios.interceptors.response.use(
+api.interceptors.response.use(
   (config) => {
     return config;
   },
   (err) => {
     if (err.response.status === 401 || err.response.status === 403) {
-      if (localStorage.getItem("accessToken")) {
-        localStorage.removeItem("accessToken");
+      if (localStorage.getItem("token")) {
+        localStorage.removeItem("token");
       }
-      // Redirecione o usuário para a página de login ou mostre uma mensagem de erro
       window.location.href = "/login";
       throw new Error(err.response.data.message);
     }

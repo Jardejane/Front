52d2b5fce8ccd9bbe31abe48d7240d307edd34cb
Jardejane <106411484+Jardejane@users.noreply.github.com>
2023-03-24 20:02:31 -0300

    add o cors

--- src/database/api.js
+++ src/database/api.js
@@ -1,18 +1,42 @@
-import axios from 'axios'
+import axios from "axios";
+axios.defaults.baseURL = "https://buddycare.onrender.com/";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers["Access-Control-Allow-Credentials"] = true;
+axios.defaults.headers["Access-Control-Allow-Origin"] =
+  "https://front-neon-ten.vercel.app";
+axios.defaults.headers["Access-Control-Allow-Methods"] =
+  "GET,PUT,POST,DELETE,PATCH,OPTIONS";
+axios.defaults.headers["Access-Control-Allow-Headers"] =
+  "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json";
+const api = axios.create({});
 
-const api = axios.create({
-    baseURL: 'https://buddycare.onrender.com',
-  });
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
-  api.interceptors.request.use(
-    (config) => {
-      const token = localStorage.getItem('token');
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+axios.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  (err) => {
+    if (err.response.status === 401 || err.response.status === 403) {
+      if (localStorage.getItem("accessToken")) {
+        localStorage.removeItem("accessToken");
       }
-      return config;
-    },
-    (error) => Promise.reject(error)
-  );
-  
-  export default api;
+      // Redirecione o usuário para a página de login ou mostre uma mensagem de erro
+      window.location.href = "/login";
+      throw new Error(err.response.data.message);
+    }
+
+    return err?.response;
+  }
+);
+
+export default api;

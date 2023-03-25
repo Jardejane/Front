34b3625e24623deeb907143def62bf7307e7fa6c
Jardejane <106411484+Jardejane@users.noreply.github.com>
2023-03-24 21:17:18 -0300

    add the axios with configure

--- src/database/api.js
+++ src/database/api.js
@@ -1,35 +1,34 @@
-import axios from "axios";
-
+import axios from 'axios'
 const api = axios.create({
     baseURL: 'https://buddycare.onrender.com/',
   });
 
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-api.interceptors.response.use(
-  (config) => {
-    return config;
-  },
-  (err) => {
-    if (err.response.status === 401 || err.response.status === 403) {
-      if (localStorage.getItem("token")) {
-        localStorage.removeItem("token");
+  api.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
       }
-      window.location.href = "/login";
-      throw new Error(err.response.data.message);
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+
+  api.interceptors.response.use(
+    (config) => {
+      return config;
+    },
+    (err) => {
+      if (err.response.status === 401 || err.response.status === 403) {
+        if (localStorage.getItem("token")) {
+          localStorage.removeItem("token");
+        }
+        window.location.href = "/login";
+        throw new Error(err.response.data.message);
+      }
+  
+      return err?.response;
     }
-
-    return err?.response;
-  }
-);
-
-export default api;
+  );
+  
+  export default api;

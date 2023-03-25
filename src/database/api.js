c194568d363fb4dced82491cf66bfb1de1685a92
Jardejane <106411484+Jardejane@users.noreply.github.com>
2023-03-25 19:47:35 -0300

    add the axios with configure

--- src/database/api.js
+++ src/database/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 const api = axios.create({
     baseURL: 'https://buddycare.onrender.com/',
   });
@@ -12,23 +13,6 @@ const api = axios.create({
       return config;
     },
     (error) => Promise.reject(error)
-  );
-
-  api.interceptors.response.use(
-    (config) => {
-      return config;
-    },
-    (err) => {
-      if (err.response.status === 401 || err.response.status === 403) {
-        if (localStorage.getItem("token")) {
-          localStorage.removeItem("token");
-        }
-        window.location.href = "/login";
-        throw new Error(err.response.data.message);
-      }
-  
-      return err?.response;
-    }
   );
   
   export default api;

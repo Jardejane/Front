49bd628afebc3f9ed0b96b230bbc9136e27eb5b8
Jardejane <106411484+Jardejane@users.noreply.github.com>
2023-03-28 00:09:15 -0300

    add the QrCode

--- src/paciente/informações-paciente/informações-paciente.js
+++ src/paciente/informações-paciente/informações-paciente.js
@@ -2,10 +2,12 @@ import "./informaçoes.css";
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { App } from "../../route/api-helpers";
+import QRCode from "qrcode.react";
 
 export const InformaçõesPaciente = (token) => {
   const navigate = useNavigate();
   const [paciente, setPaciente] = useState({});
+  const [qrCodeUrl, setQrCodeUrl] = useState("");
   const { id } = useParams();
 
   async function getPacienteById(id) {
@@ -14,11 +16,11 @@ export const InformaçõesPaciente = (token) => {
     console.log("paciente", paciente);
   }
 
-  const LinkUrl = async (id) => {
+  const generateQRCode = async (id) => {
     const req = await App.getPacienteSalaId(token, id);
     const { patient } = req;
     const doctorConnectionLink = `https://webchat-grupotech.vercel.app/${patient}`;
-    window.open(doctorConnectionLink, "_blank");
+    setQrCodeUrl(doctorConnectionLink);
   };
 
   const deletePaciente = async (id) => {
@@ -32,6 +34,7 @@ export const InformaçõesPaciente = (token) => {
 
   useEffect(() => {
     getPacienteById(id);
+    generateQRCode(id);
   }, [id]);
 
   return (
@@ -56,10 +59,11 @@ export const InformaçõesPaciente = (token) => {
           <button onClick={() => deletePaciente(id)}>excluir</button>
         </div>
       </div>
-      <div>
-        <button onClick={() => LinkUrl(id)}>
-          link 
-        </button>
+      <div className="qrcode-container">
+        <QRCode value={qrCodeUrl} size={256} />
+      
+        <a href={qrCodeUrl} target="_blank" rel="noopener noreferrer">Link para o chat do paciente</a>
+
       </div>
     </div>
   );

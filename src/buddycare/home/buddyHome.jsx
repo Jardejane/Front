ec131549819ae9ac0883adc1d3467c9c64a90e43
Jardejane <106411484+Jardejane@users.noreply.github.com>
2023-03-30 15:38:35 -0300

    add the function pesquise

--- src/buddycare/home/buddyHome.jsx
+++ src/buddycare/home/buddyHome.jsx
@@ -1,52 +1,73 @@
-import './style.css'
+import "./style.css";
 import React, { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
-import { App } from '../../route/api-helpers';
+import { useNavigate } from "react-router-dom";
+import { App } from "../../route/api-helpers";
 
 export const BuddyHome = () => {
-    const [doctor, setDoctor] = useState([]);
-    const [patients, setPatients] = useState([]);
-    const navigate = useNavigate();
-  
-    async function getAllDoctor() {
-      const doctors = await App.getAllPsicologoNaBuddy();
-      console.log("Doctor",doctors)
-      setDoctor(doctors);
-      const filteredPatients = patients.filter((patient) => {
-        return patient.doctor_id === doctors.id;
-      });
-      setPatients(filteredPatients);
-    }
-  
-    useEffect(() => {
-      getAllDoctor();
-    }, []);
+  const [doctor, setDoctor] = useState([]);
+  const [patients, setPatients] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const navigate = useNavigate();
 
-    return (
-      <div className="page-container3">
-        <h1>Doctors</h1>
-        <div className="results-container">
-          {doctor.map((doctor) => (
-            <div
-              className="card1"
-              key={doctor.id}
-              onClick={() => navigate(`/doctor/${doctor.id}`)}
-            >
-              <img
-                className="card-image"
-                src={doctor.photo}
-                alt={doctor.name}
-              />
-              <div className="card-info">
-                <h2>{doctor.name}</h2>
-                <p>CRP: {doctor.crp}</p>
-                <p>Email: {doctor.email}</p>
-                <p>Pacientes: {doctor._count.patients}</p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
+  async function getAllDoctor() {
+    const doctors = await App.getAllPsicologoNaBuddy();
+    console.log("Doctor", doctors);
+    setDoctor(doctors);
+    const filteredPatients = patients.filter((patient) => {
+      return patient.doctor_id === doctors.id;
+    });
+    setPatients(filteredPatients);
+  }
+
+  function searchDoctors() {
+    return doctor.filter((doctor) =>
+      doctor.name.toLowerCase().includes(searchValue.toLowerCase())
     );
-  
-}
+  }
+
+  useEffect(() => {
+    getAllDoctor();
+  }, []);
+
+  return (
+    <div className="page-container5">
+      <h1>Doctors</h1>
+      <div className="search-page-container-Pesquisa">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Pesquisar por nome"
+          />
+          <button type="submit">Pesquisar</button>
+        </form>
+      </div>
+      <div className="results-container">
+        {searchDoctors().map((doctor) => (
+          <div
+            className="card1"
+            key={doctor.id}
+            onClick={() => navigate(`/doctor/${doctor.id}`)}
+          >
+            <img
+              className="card-image"
+              src={doctor.photo}
+              alt={doctor.name}
+            />
+            <div className="card-info">
+              <h2>{doctor.name}</h2>
+              <p>CRP: {doctor.crp}</p>
+              <p>Email: {doctor.email}</p>
+              <p>Pacientes: {doctor._count.patients}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
